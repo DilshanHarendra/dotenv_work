4d2e34d4ffd264b706799428bd75af5cc3066078
DilshanHarendra <harendracripto@gmail.com>
2021-10-26 15:28:42 +0530

    ads rempo variables

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ Vue.config.productionTip = false
 
 console.log(process.env.VUE_APP_NAME)
 console.log(process.env.VUE_APP_USER)
+console.log(process.env.VUE_APP_MY_NAME)
 
 new Vue({
   render: h => h(App),
